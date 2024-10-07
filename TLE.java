9d228b4217365df7419432f58bba5210396f9664
Sulabh <sulabhambule@gmail.com>
2024-10-07 17:21:16 +0530

    done

--- TLE.java
+++ TLE.java
@@ -9,12 +9,12 @@ public class TLE {
   static FastReader in = new FastReader();
 
   public static void main(String[] args) throws Exception {
+    init();
     int cases = in.nextInt();
     while (cases-- > 0) {
       Accepted();
     }
-
-    // out.flush();
+    out.flush();
     out.close();
   }
 
@@ -26,24 +26,67 @@ public class TLE {
    * 
    */
 
+  static int max = (int) 1e5;
+  static ArrayList<Integer>[] factors = new ArrayList[max + 5];
+
+  private static void init() {
+    for (int i = 0; i <= max; i++) {
+      factors[i] = new ArrayList<>();
+    }
+
+    for (int i = 1; i <= max; i++) {
+      for (int j = i; j <= max; j += i) {
+        factors[j].add(i);
+      }
+    }
+  }
+
   private static void Accepted() {
     int n = in.nextInt();
     int m = in.nextInt();
-
     int[] a = new int[n];
     for (int i = 0; i < a.length; i++) {
       a[i] = in.nextInt();
     }
 
-  }
+    Arrays.sort(a);
 
-  /*
-   *  
-   * 
-   * 
-   * 
-   * 
-   */
+    int l = 0;
+    int ans = Integer.MAX_VALUE;
+    int count = 0;
+    int[] freq = new int[max + 1];
+
+    for (int r = 0; r < n; r++) {
+      for (int x : factors[a[r]]) {
+        if (x > m)
+          break;
+        freq[x]++;
+        if (freq[x] == 1) {
+          count++;
+        }
+      }
+
+      while (count == m) {
+        ans = Math.min(ans, a[r] - a[l]);
+
+        for (int x : factors[a[l]]) {
+          if (x > m)
+            break;
+          freq[x]--;
+          if (freq[x] == 0) {
+            count--;
+          }
+        }
+        l++;
+      }
+    }
+
+    if (ans == Integer.MAX_VALUE) {
+      out.println(-1);
+    } else {
+      out.println(ans);
+    }
+  }
 
   static class FastReader {
     BufferedReader br;
