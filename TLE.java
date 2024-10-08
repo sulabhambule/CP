a6f671285b74155bf4ac9cfe4b312566b612fe0b
Sulabh <sulabhambule@gmail.com>
2024-10-08 14:38:34 +0530

    done

--- TLE.java
+++ TLE.java
@@ -9,9 +9,8 @@ public class TLE {
   static FastReader in = new FastReader();
 
   public static void main(String[] args) throws Exception {
-    init();
-    int cases = in.nextInt();
-    while (cases-- > 0) {
+    int cf = in.nextInt();
+    while (cf-- > 0) {
       Accepted();
     }
     out.flush();
@@ -25,67 +24,39 @@ public class TLE {
    * 
    * 
    */
-
-  static int max = (int) 1e5;
-  static ArrayList<Integer>[] factors = new ArrayList[max + 5];
-
-  private static void init() {
-    for (int i = 0; i <= max; i++) {
-      factors[i] = new ArrayList<>();
-    }
-
-    for (int i = 1; i <= max; i++) {
-      for (int j = i; j <= max; j += i) {
-        factors[j].add(i);
-      }
-    }
-  }
-
   private static void Accepted() {
     int n = in.nextInt();
-    int m = in.nextInt();
-    int[] a = new int[n];
-    for (int i = 0; i < a.length; i++) {
-      a[i] = in.nextInt();
+    long[] h = new long[n];
+    for (int i = 0; i < h.length; i++) {
+      h[i] = in.nextLong();
     }
-
-    Arrays.sort(a);
-
-    int l = 0;
-    int ans = Integer.MAX_VALUE;
-    int count = 0;
-    int[] freq = new int[max + 1];
-
-    for (int r = 0; r < n; r++) {
-      for (int x : factors[a[r]]) {
-        if (x > m)
-          break;
-        freq[x]++;
-        if (freq[x] == 1) {
-          count++;
-        }
-      }
-
-      while (count == m) {
-        ans = Math.min(ans, a[r] - a[l]);
-
-        for (int x : factors[a[l]]) {
-          if (x > m)
-            break;
-          freq[x]--;
-          if (freq[x] == 0) {
-            count--;
-          }
-        }
-        l++;
+    long ans = -1;
+    long low = 0;
+    long high = (long) 1e9;
+    while (low <= high) {
+      long mid = (low + high) / 2;
+      if (isPossible(mid, h)) {
+        ans = mid;
+        low = mid + 1;
+      } else {
+        high = mid - 1;
       }
     }
+    out.println(ans);
+  }
 
-    if (ans == Integer.MAX_VALUE) {
-      out.println(-1);
-    } else {
-      out.println(ans);
+  private static boolean isPossible(long x, long[] h) {
+    int n = h.length;
+    long[] cur_h = h.clone();
+    for (int i = n - 1; i >= 2; --i) {
+      if (cur_h[i] < x) {
+        return false;
+      }
+      long d = Math.min(h[i], cur_h[i] - x) / 3;
+      cur_h[i - 1] += d;
+      cur_h[i - 2] += 2 * d;
     }
+    return cur_h[0] >= x && cur_h[1] >= x;
   }
 
   static class FastReader {
