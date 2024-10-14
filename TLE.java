058d664c874eafd166c01097e495fe3e3d1b5905
Sulabh <sulabhambule@gmail.com>
2024-10-14 23:44:13 +0530

    new chnge

--- TLE.java
+++ TLE.java
@@ -1,20 +1,19 @@
-
 import java.io.*;
 import java.util.*;
 
 // Author : Sulabh Ambule
 public class TLE {
-
   public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
   static long MOD = (long) (1e9 + 7);
   // static long MOD = 998244353;
+  // Codeforces Edu-Round 170 (Div 2)
   static FastReader in = new FastReader();
 
   public static void main(String[] args) throws Exception {
-    int cf = in.nextInt();
-    while (cf-- > 0) {
-      Accepted();
-    }
+    // int cf = in.nextInt();
+    // while (cf-- > 0) {
+    Accepted();
+    // }
     out.flush();
     out.close();
   }
@@ -26,7 +25,34 @@ public class TLE {
    */
 
   private static void Accepted() {
-    
+    int t = in.nextInt();
+    int[] a = new int[t];
+    for (int i = 0; i < a.length; i++) {
+      a[i] = in.nextInt();
+    }
+
+    int[] b = new int[t];
+    for (int i = 0; i < a.length; i++) {
+      b[i] = in.nextInt();
+    }
+
+    for (int i = 0; i < t; i++) {
+      System.out.println(binPow(2, b[i], MOD));
+    }
+  }
+
+  static long binPow(long a, long b, long MOD) {
+    long result = 1;
+    a = a % MOD; // in case 'a' is larger than MOD
+
+    while (b > 0) {
+      if ((b & 1) == 1) {
+        result = (result * a) % MOD;
+      }
+      a = (a * a) % MOD;
+      b >>= 1;
+    }
+    return result;
   }
 
   static class FastReader {
