3d1b4b4ee7a430ac03abed2b9a4eda89daf8f1ca
Sulabh <sulabhambule@gmail.com>
2025-02-09 19:57:54 +0530

    added new  codes

--- Simple.java
+++ Simple.java
@@ -15,6 +15,11 @@ public class Simple {
     }
 
     static void solve() {
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
         
     }
 
