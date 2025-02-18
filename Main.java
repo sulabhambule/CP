b709ab79697ffb41659ed67fec5ab074ddb61698
Sulabh <sulabhambule@gmail.com>
2025-02-18 15:38:34 +0530

    added new codes

--- Main.java
+++ Main.java
@@ -1,17 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Scanner in = new Scanner(System.in);
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    static FASTIO in = new FASTIO();
 
-    static void subh() {
+    public static void main(String[] Hi) throws IOException {
+        int cp = in.nextInt();
+        while (cp-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+    static void solve() {
+        int n = in.nextInt();
 
     }
 
-    public static void main(String[] args) {
-        int t = in.nextInt();
-        while (t-- > 0) {
-            subh();
+    static class FASTIO {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FASTIO() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
-        in.close();
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
     }
 }
