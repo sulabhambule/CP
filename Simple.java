3eb6e49f9480ae4b0a4a8d2fb3d4b326dc77dd0e
Sulabh <sulabhambule@gmail.com>
2025-03-21 11:15:58 +0530

    added new codes

--- Simple.java
+++ Simple.java
@@ -2,167 +2,112 @@ import java.io.*;
 import java.util.*;
 
 public class Simple {
-     public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
-     static FASTIO in = new FASTIO();
-     static final int MOD = 998244353;
+     public static void main(String[] args) throws IOException {
+          FastIO in = new FastIO();
+          PrintWriter out = new PrintWriter(System.out);
 
-     public static void main(String[] Hi) throws IOException {
-          int cp = 1;
-          while (cp-- > 0) {
-               solve();
+          int n = in.nextInt();
+          int[] arr = new int[n];
+
+          for (int i = 0; i < n; i++) {
+               arr[i] = in.nextInt();
+          }
+
+          int[] a = new int[n];
+          Arrays.fill(a, 1);
+          int[] ans = new int[n];
+
+          SegTree tree = new SegTree(n, a);
+
+          for (int i = n - 1; i >= 0; i--) {
+               int count = arr[i] + 1;
+               int index = tree.findKthOne(count);
+               ans[index] = i + 1;
+               tree.flip(index);
+          }
+
+          for (int i = 0; i < n; i++) {
+               out.print(ans[i] + " ");
           }
           out.close();
-     }
-
-     static void solve() {
-          int n = in.nextInt();
-          long[] p = new long[n];
-          for (int i = 0; i < n; i++) {
-               p[i] = in.nextLong() - 1;
-          }
-          SegTree tree = new SegTree(n, new long[n]);
-          int[] ans = new int[n];
-          for (int i = 0; i < n; i++) {
-               int num = (int) p[i];
-               if (num == n - 1) {
-                    ans[i] = 0;
-               } else {
-                    ans[i] = (int) tree.makeQuery(num + 1, n - 1).val;
-               }
-               tree.makeUpdate(num, 1);
-          }
-          for (int i : ans) {
-               out.print(i + " ");
-          }
-          out.println();
-     }
-
-     static class FASTIO {
-          BufferedReader br;
-          StringTokenizer st;
-
-          public FASTIO() {
-               br = new BufferedReader(new InputStreamReader(System.in));
-          }
-
-          String next() {
-               while (st == null || !st.hasMoreElements()) {
-                    try {
-                         st = new StringTokenizer(br.readLine());
-                    } catch (IOException e) {
-                         e.printStackTrace();
-                    }
-               }
-               return st.nextToken();
-          }
-
-          int nextInt() {
-               return Integer.parseInt(next());
-          }
-
-          long nextLong() {
-               return Long.parseLong(next());
-          }
-
-          double nextDouble() {
-               return Double.parseDouble(next());
-          }
      }
 }
 
 class SegTree {
-     private Node[] tree;
-     private long[] arr;
+     private int[] tree;
+     private int[] arr;
      private int n;
-     private int s;
 
-     public SegTree(int a_len, long[] a) {
-          this.arr = a;
-          this.n = a_len;
-          this.s = 1;
-          while (s < 2 * n) {
-               s = s << 1;
-          }
-          tree = new Node[s];
-          Arrays.fill(tree, new Node());
-          build(0, n - 1, 1);
+     public SegTree(int size, int[] array) {
+          this.n = size;
+          this.arr = array;
+          this.tree = new int[4 * n];
+          build(0, 0, n - 1);
      }
 
-     private void build(int start, int end, int index) {
+     private void build(int node, int start, int end) {
           if (start == end) {
-               tree[index] = new Node(arr[start]);
+               tree[node] = arr[start];
                return;
           }
           int mid = (start + end) / 2;
-          build(start, mid, 2 * index);
-          build(mid + 1, end, 2 * index + 1);
-          tree[index] = new Node();
-          tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+          build(2 * node + 1, start, mid);
+          build(2 * node + 2, mid + 1, end);
+          tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
      }
 
-     private void update(int start, int end, int index, int queryIndex, Update u) {
+     private void update(int node, int start, int end, int idx) {
           if (start == end) {
-               u.apply(tree[index]);
+               arr[idx] = 0; // Mark the position as used (flip 1 → 0)
+               tree[node] = 0;
                return;
           }
           int mid = (start + end) / 2;
-          if (mid >= queryIndex) {
-               update(start, mid, 2 * index, queryIndex, u);
+          if (idx <= mid) {
+               update(2 * node + 1, start, mid, idx);
           } else {
-               update(mid + 1, end, 2 * index + 1, queryIndex, u);
+               update(2 * node + 2, mid + 1, end, idx);
           }
-          tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+          tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
      }
 
-     private Node query(int start, int end, int index, int left, int right) {
-          if (start > right || end < left) {
-               return new Node();
-          }
-          if (start >= left && end <= right) {
-               return tree[index];
-          }
-          int mid = (start + end) / 2;
-          Node l = query(start, mid, 2 * index, left, right);
-          Node r = query(mid + 1, end, 2 * index + 1, left, right);
-          Node ans = new Node();
-          ans.merge(l, r);
-          return ans;
+     public void flip(int index) {
+          update(0, 0, n - 1, index);
      }
 
-     public void makeUpdate(int index, long val) {
-          Update newUpdate = new Update(val);
-          update(0, n - 1, 1, index, newUpdate);
+     private int findKthOne(int node, int start, int end, int k) {
+          if (start == end)
+               return start;
+
+          int leftCount = tree[2 * node + 1];
+          if (k > leftCount) {
+               return findKthOne(2 * node + 2, (start + end) / 2 + 1, end, k - leftCount);
+          } else {
+               return findKthOne(2 * node + 1, start, (start + end) / 2, k);
+          }
      }
 
-     public Node makeQuery(int left, int right) {
-          return query(0, n - 1, 1, left, right);
+     public int findKthOne(int k) {
+          return findKthOne(0, 0, n - 1, k);
      }
 }
 
-class Node {
-     long val;
+class FastIO {
+     BufferedReader br;
+     StringTokenizer st;
 
-     public Node() {
-          this.val = 0;
+     public FastIO() {
+          br = new BufferedReader(new InputStreamReader(System.in));
      }
 
-     public Node(long p1) {
-          this.val = p1;
+     String next() throws IOException {
+          while (st == null || !st.hasMoreElements()) {
+               st = new StringTokenizer(br.readLine());
+          }
+          return st.nextToken();
      }
 
-     public void merge(Node l, Node r) {
-          this.val = l.val + r.val;
-     }
-}
-
-class Update {
-     long val;
-
-     public Update(long p1) {
-          this.val = p1;
-     }
-
-     public void apply(Node a) {
-          a.val = val;
+     int nextInt() throws IOException {
+          return Integer.parseInt(next());
      }
 }
