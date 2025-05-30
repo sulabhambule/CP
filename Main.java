3ec29531532258717ddc0f593c4fd2a9234eb804
Sulabh <sulabhambule@gmail.com>
2025-05-30 09:11:36 +0530

    added new nodes

--- Main.java
+++ Main.java
@@ -3,28 +3,139 @@ import java.util.*;
 
 public class Main {
   public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
-  static FastReader in = new FastReader();
-  static int mod = (int) 1e9 + 7;
-  static final int inf = (int) 1e9;
-  static final long INF = (long) 1e18;
+  static FASTIO in = new FASTIO();
+  static List<Integer>[] adj;
+  static int[] color;
+  static int[] inTime, outTime;
+  static int timer = 0;
+  static int n;
 
-  public static void main(String[] args) {
-    int t = 1;
-    while (t-- > 0) {
-      solve();
+  public static void main(String[] args) throws IOException {
+    n = in.nextInt();
+    color = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+      color[i] = in.nextInt();
     }
-    out.flush();
+    adj = new ArrayList[n + 1];
+    for (int i = 0; i <= n; i++)
+      adj[i] = new ArrayList<>();
+    for (int i = 0; i < n - 1; i++) {
+      int u = in.nextInt(), v = in.nextInt();
+      adj[u].add(v);
+      adj[v].add(u);
+    }
+    inTime = new int[n + 1];
+    outTime = new int[n + 1];
+    dfs(1, 0);
+
+    // Prepare queries: for each node, we want distinct count in [inTime[node],
+    // outTime[node]]
+    // We'll process times 0 to 2*n-1, update last occurrence of color
+    int size = 2 * n;
+    Query[] queries = new Query[n];
+    for (int i = 1; i <= n; i++) {
+      queries[i - 1] = new Query(inTime[i], outTime[i], i);
+    }
+    Arrays.sort(queries, Comparator.comparingInt(qr -> qr.r));
+
+    SegTree seg = new SegTree();
+    seg.init(size);
+    // last occurrence map
+    Map<Integer, Integer> last = new HashMap<>();
+    int idx = 0;
+    int[] answer = new int[n + 1];
+    for (int t = 0; t < size; t++) {
+      // at time t, find which node appears entering or exiting
+      int nodeAtTime = nodeAt[t];
+      if (nodeAtTime != 0 && (inTime[nodeAtTime] == t)) {
+        int c = color[nodeAtTime];
+        // remove old
+        if (last.containsKey(c)) {
+          seg.update(0, size - 1, last.get(c), 0, 0);
+        }
+        // add new
+        seg.update(0, size - 1, t, 0, 1);
+        last.put(c, t);
+      }
+      // answer queries with r == t
+      while (idx < n && queries[idx].r == t) {
+        answer[queries[idx].id] = seg.query(0, size - 1, queries[idx].l, queries[idx].r, 0);
+        idx++;
+      }
+    }
+
+    for (int i = 1; i <= n; i++) {
+      out.print(answer[i]);
+      out.print(i < n ? ' ' : '\n');
+    }
+    out.close();
   }
 
-  static void solve() {
-    
+  // Euler tour to assign in and out times and record node at each time
+  static int[] nodeAt;
+
+  private static void dfs(int u, int p) {
+    if (nodeAt == null)
+      nodeAt = new int[2 * n];
+    inTime[u] = timer;
+    nodeAt[timer++] = u;
+    for (int v : adj[u]) {
+      if (v == p)
+        continue;
+      dfs(v, u);
+    }
+    outTime[u] = timer;
+    nodeAt[timer++] = 0; // no entry on exit
   }
 
-  static class FastReader {
+  static class Query {
+    int l, r, id;
+
+    Query(int l, int r, int id) {
+      this.l = l;
+      this.r = r;
+      this.id = id;
+    }
+  }
+
+  static class SegTree {
+    private int n;
+    public int[] st;
+
+    public void init(int n) {
+      this.n = n;
+      st = new int[4 * n];
+    }
+
+    public void update(int start, int end, int idx, int node, int value) {
+      if (start == end) {
+        st[node] = value;
+        return;
+      }
+      int mid = (start + end) >> 1;
+      if (idx <= mid)
+        update(start, mid, idx, 2 * node + 1, value);
+      else
+        update(mid + 1, end, idx, 2 * node + 2, value);
+      st[node] = st[2 * node + 1] + st[2 * node + 2];
+    }
+
+    public int query(int start, int end, int l, int r, int node) {
+      if (start > r || end < l)
+        return 0;
+      if (start >= l && end <= r)
+        return st[node];
+      int mid = (start + end) >> 1;
+      return query(start, mid, l, r, 2 * node + 1)
+          + query(mid + 1, end, l, r, 2 * node + 2);
+    }
+  }
+
+  static class FASTIO {
     BufferedReader br;
     StringTokenizer st;
 
-    public FastReader() {
+    FASTIO() {
       br = new BufferedReader(new InputStreamReader(System.in));
     }
 
--- fast.cpp
+++ fast.cpp
@@ -1,72 +1,133 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const long long LINF = 1e18;
+const int MAXN = 2e5 + 5;
 
-vector<vector<int>> adj;
-vector<vector<int>> dp;
+int n, timer = 0;
+vector<int> adj[MAXN];
+int color[MAXN];
+int inTime[MAXN], outTime[MAXN];
+int nodeAt[2 * MAXN]; // Euler tour order
+int answer[MAXN];
 
-void dfs(int node, int parent)
+struct Query
 {
-    for (int adjNode : adj[node])
+    int l, r, id;
+    bool operator<(const Query &other) const
     {
-        if (adjNode != parent)
-        {
-            dfs(adjNode, node);
-        }
+        return r < other.r;
     }
+};
 
-    int total = 0;
-    for (int adjNode : adj[node])
+void dfs(int u, int p)
+{
+    inTime[u] = timer;
+    nodeAt[timer++] = u;
+    for (int v : adj[u])
     {
-        if (adjNode != parent)
-        {
-            total += max(dp[adjNode][0], dp[adjNode][1]);
-        }
-    }
-    dp[node][0] = total;
-
-    for (int adjNode : adj[node])
-    {
-        if (adjNode == parent)
+        if (v == p)
             continue;
-
-        int current = 1 + dp[adjNode][0];
-        for (int v : adj[node])
-        {
-            if (v == parent || v == adjNode)
-                continue;
-            current += max(dp[v][0], dp[v][1]);
-        }
-
-        dp[node][1] = max(dp[node][1], current);
+        dfs(v, u);
     }
+    outTime[u] = timer;
+    nodeAt[timer++] = 0; // 0 means exit or dummy
 }
+
+struct SegTree
+{
+    int size;
+    vector<int> st;
+
+    void init(int n)
+    {
+        size = n;
+        st.assign(4 * n, 0);
+    }
+
+    void update(int node, int l, int r, int idx, int val)
+    {
+        if (l == r)
+        {
+            st[node] = val;
+            return;
+        }
+        int mid = (l + r) / 2;
+        if (idx <= mid)
+            update(2 * node + 1, l, mid, idx, val);
+        else
+            update(2 * node + 2, mid + 1, r, idx, val);
+        st[node] = st[2 * node + 1] + st[2 * node + 2];
+    }
+
+    int query(int node, int l, int r, int ql, int qr)
+    {
+        if (r < ql || l > qr)
+            return 0;
+        if (ql <= l && r <= qr)
+            return st[node];
+        int mid = (l + r) / 2;
+        return query(2 * node + 1, l, mid, ql, qr) + query(2 * node + 2, mid + 1, r, ql, qr);
+    }
+};
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0);
 
-    int n;
     cin >> n;
-
-    adj.assign(n + 1, vector<int>());
-    dp.assign(n + 1, vector<int>(2, 0));
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> color[i];
+    }
 
     for (int i = 0; i < n - 1; ++i)
     {
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
     dfs(1, 0);
 
-    cout << max(dp[1][0], dp[1][1]) << '\n';
+    vector<Query> queries;
+    for (int i = 1; i <= n; ++i)
+    {
+        queries.push_back({inTime[i], outTime[i], i});
+    }
+
+    sort(queries.begin(), queries.end());
+
+    SegTree seg;
+    seg.init(2 * n);
+    unordered_map<int, int> last;
+    int idx = 0;
+
+    for (int t = 0; t < 2 * n; ++t)
+    {
+        int node = nodeAt[t];
+        if (node != 0 && inTime[node] == t)
+        {
+            int c = color[node];
+            if (last.count(c))
+            {
+                seg.update(0, 0, 2 * n - 1, last[c], 0);
+            }
+            seg.update(0, 0, 2 * n - 1, t, 1);
+            last[c] = t;
+        }
+        while (idx < n && queries[idx].r == t)
+        {
+            answer[queries[idx].id] = seg.query(0, 0, 2 * n - 1, queries[idx].l, queries[idx].r);
+            ++idx;
+        }
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << answer[i] << (i < n ? ' ' : '\n');
+    }
 
     return 0;
 }
