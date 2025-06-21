4e4d4ce73e7ff42fe3210a866fced50cf64b508e
Sulabh <sulabhambule@gmail.com>
2025-06-21 14:10:56 +0530

    added new template codes

--- Main.java
+++ Main.java
@@ -4,30 +4,12 @@ import java.util.*;
 public class Main {
   static FastReader in = new FastReader();
   static PrintWriter out = new PrintWriter(System.out);
+  static int mod = (int) 1e9 + 7;
 
   public static void main(String[] args) {
     int t = in.nextInt();
     while (t-- > 0) {
-      int n = in.nextInt();
-      long sum = 0;
-      long[] a = new long[n];
-      for (int i = 0; i < n; i++) {
-        a[i] = in.nextLong();
-        sum += a[i];
-      }
-      HashMap<Long, Integer> map = new HashMap<>();
-      long ans = 0;
-      long prod = sum * (n - 2);
-      if (prod % n != 0) {
-        out.println(0);
-        continue;
-      }
-      long tar = sum - (prod / n);
-      for (long i : a) {
-        ans += map.getOrDefault(tar - i, 0);
-        map.put(i, map.getOrDefault(i, 0) + 1);
-      }
-      out.println(ans);
+
     }
     out.flush();
   }
